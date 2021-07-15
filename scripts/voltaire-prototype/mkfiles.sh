obsidiansystems/cardano-node

commit b0c2ceeffe4f6cc535ec1a47d8d4b232296402f0

mkfiles.sh: use GNU "sed" syntax

scripts/voltaire-prototype/mkfiles.sh
@@ -25,10 +25,10 @@ fi
 # copy and tweak the configuration
 cp configuration/defaults/byron-mainnet/configuration.yaml ${ROOT}/
 
-sed -e 's/Protocol: RealPBFT/Protocol: Voltaire/' \
+sed -i ${ROOT}/configuration.yaml
+    -e 's/Protocol: RealPBFT/Protocol: Voltaire/' \
     -e 's/minSeverity: Info/minSeverity: Warning/'\
     -e 's/LastKnownBlockVersion-Minor: 2/LastKnownBlockVersion-Minor: 0/'\
-    -i '' ${ROOT}/configuration.yaml
 
 # Set up our template
 cardano-cli genesis create --testnet-magic 42 --genesis-dir ${ROOT}
@@ -38,13 +38,13 @@ cardano-cli genesis create --testnet-magic 42 --genesis-dir ${ROOT}
 # We're going to use really quick epochs (300 seconds), by using short slots 0.2s
 # and K=10, but we'll keep long KES periods so we don't have to bother
 # cycling KES keys
-sed -e 's/"slotLength": 1/"slotLength": 0.2/' \
+sed -i ${ROOT}/genesis.spec.json
+    -e 's/"slotLength": 1/"slotLength": 0.2/' \
     -e 's/"activeSlotsCoeff": 5.0e-2/"activeSlotsCoeff": 0.1/' \
     -e 's/"securityParam": 2160/"securityParam": 10/' \
     -e 's/"epochLength": 432000/"epochLength": 1500/' \
     -e 's/"decentralisationParam": 1/"decentralisationParam": 0.7/' \
     -e 's/"updateQuorum": 5/"updateQuorum": 2/'\
-    -i '' ${ROOT}/genesis.spec.json
 
 # Now generate for real:
 
@@ -56,8 +56,8 @@ cardano-cli genesis create \
     --supply "$INITIAL_FUNDS"
 
 # Increase the total supply, so that there's some funds left in the reserves
-sed -e "s/\"maxLovelaceSupply\": $INITIAL_FUNDS/\"maxLovelaceSupply\": $SUPPLY/" \
-    -i '' ${ROOT}/genesis.json
+sed -i ${ROOT}/genesis.json
+    -e "s/\"maxLovelaceSupply\": $INITIAL_FUNDS/\"maxLovelaceSupply\": $SUPPLY/" \
 
 pushd ${ROOT}
 

commit 798ac32bf37ac0e1aaa8b0d5068be1b950400134

unbreak mkfiles.sh
broken by b0c2ceeffe4f6cc535ec1a47d8d4b232296402f0

scripts/voltaire-prototype/mkfiles.sh
@@ -25,10 +25,10 @@ fi
 # copy and tweak the configuration
 cp configuration/defaults/byron-mainnet/configuration.yaml ${ROOT}/
 
-sed -i ${ROOT}/configuration.yaml
+sed -i ${ROOT}/configuration.yaml \
     -e 's/Protocol: RealPBFT/Protocol: Voltaire/' \
     -e 's/minSeverity: Info/minSeverity: Warning/'\
-    -e 's/LastKnownBlockVersion-Minor: 2/LastKnownBlockVersion-Minor: 0/'\
+    -e 's/LastKnownBlockVersion-Minor: 2/LastKnownBlockVersion-Minor: 0/'
 
 # Set up our template
 cardano-cli genesis create --testnet-magic 42 --genesis-dir ${ROOT}
@@ -38,13 +38,13 @@ cardano-cli genesis create --testnet-magic 42 --genesis-dir ${ROOT}
 # We're going to use really quick epochs (300 seconds), by using short slots 0.2s
 # and K=10, but we'll keep long KES periods so we don't have to bother
 # cycling KES keys
-sed -i ${ROOT}/genesis.spec.json
+sed -i ${ROOT}/genesis.spec.json \
     -e 's/"slotLength": 1/"slotLength": 0.2/' \
     -e 's/"activeSlotsCoeff": 5.0e-2/"activeSlotsCoeff": 0.1/' \
     -e 's/"securityParam": 2160/"securityParam": 10/' \
     -e 's/"epochLength": 432000/"epochLength": 1500/' \
     -e 's/"decentralisationParam": 1/"decentralisationParam": 0.7/' \
-    -e 's/"updateQuorum": 5/"updateQuorum": 2/'\
+    -e 's/"updateQuorum": 5/"updateQuorum": 2/'
 
 # Now generate for real:
 
@@ -56,8 +56,8 @@ cardano-cli genesis create \
     --supply "$INITIAL_FUNDS"
 
 # Increase the total supply, so that there's some funds left in the reserves
-sed -i ${ROOT}/genesis.json
-    -e "s/\"maxLovelaceSupply\": $INITIAL_FUNDS/\"maxLovelaceSupply\": $SUPPLY/" \
+sed -i ${ROOT}/genesis.json \
+    -e "s/\"maxLovelaceSupply\": $INITIAL_FUNDS/\"maxLovelaceSupply\": $SUPPLY/"
 
 pushd ${ROOT}
 