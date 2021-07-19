23fd1f1a707b5accb04024196cd4b3e6871639ab
Rune K. Svendsen <runesvend@gmail.com>
2021-07-19 11:26:12 +0200

    revert 14bb6c09ec4622b53f6aea8ac0a44657fcdc3c27

--- scripts/voltaire-prototype/mkfiles.sh
+++ scripts/voltaire-prototype/mkfiles.sh
@@ -301,14 +301,14 @@ fi
 for NODE in ${ALL_NODES}; do
 
 COMMAND=$(cat <<-END
-cardano-node run ^
---config ${ROOT}/configuration.yaml ^
---topology ${ROOT}/${NODE}/topology.json ^
---database-path ${ROOT}/${NODE}/db ^
---socket-path ${WINDOWS_SOCKET_PREFIX}${NODE} ^
---shelley-kes-key ${ROOT}/${NODE}/kes.skey ^
---shelley-vrf-key ${ROOT}/${NODE}/vrf.skey ^
---shelley-operational-certificate ${ROOT}/${NODE}/node.cert ^
+cardano-node run \\
+--config ${ROOT}/configuration.yaml \\
+--topology ${ROOT}/${NODE}/topology.json \\
+--database-path ${ROOT}/${NODE}/db \\
+--socket-path ${WINDOWS_SOCKET_PREFIX}${NODE} \\
+--shelley-kes-key ${ROOT}/${NODE}/kes.skey \\
+--shelley-vrf-key ${ROOT}/${NODE}/vrf.skey \\
+--shelley-operational-certificate ${ROOT}/${NODE}/node.cert \\
 --port $(cat "${NODE}"/port)
 END
 )
