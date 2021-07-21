355b579cfde75a1b717685ec7776ae0e4ae45c3b
Rune K. Svendsen <runesvend@gmail.com>
2021-07-21 15:25:54 +0200

    CI/MIR proposal: halve epoch duration

--- scripts/voltaire-prototype/mkfiles.sh
+++ scripts/voltaire-prototype/mkfiles.sh
@@ -51,11 +51,11 @@ cardano-cli genesis create --testnet-magic 42 --genesis-dir ${ROOT}
 
 # Then edit the genesis.spec.json ...
 
-# We're going to use really quick epochs (300 seconds), by using short slots 0.2s
+# We're going to use really quick epochs (150 seconds), by using short slots 0.1s
 # and K=10, but we'll keep long KES periods so we don't have to bother
 # cycling KES keys
 sed_inplace ${ROOT}/genesis.spec.json \
-    -e 's/"slotLength": 1/"slotLength": 0.2/' \
+    -e 's/"slotLength": 1/"slotLength": 0.1/' \
     -e 's/"activeSlotsCoeff": 5.0e-2/"activeSlotsCoeff": 0.1/' \
     -e 's/"securityParam": 2160/"securityParam": 10/' \
     -e 's/"epochLength": 432000/"epochLength": 1500/' \
